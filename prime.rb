652e14d7aee34553de24deedeb8e617ac8f203b0
Aaron Na <syn228@nyu.edu>
2018-04-10 20:52:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,11 @@ def prime?(number)
   if number <= 0 || number == 1 || number == 2
     return false
   else
-    while i <= number/2
-    if number % i == 0
+    while a <= number/2
+    if number % a == 0
       return false
     end
-    i+= 1
+    a+= 1
   end
     return true
   end
