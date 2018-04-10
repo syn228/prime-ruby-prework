721674006cc1de5fefdfa41c0c4dc2faaef65479
Aaron Na <syn228@nyu.edu>
2018-04-10 20:53:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+  a = 2
+  if number <= 0 || number == 1 || number == 2
+    return false
+  else
+    while a <= number/2
+    if number % a == 0
+      return false
+    end
+    a+= 1
+  end
+    return true
+  end
+end
