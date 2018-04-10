7ae090bf5cc6d8f8712e292ec616b196520a2b78
Aaron Na <syn228@nyu.edu>
2018-04-10 20:50:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 def prime?(number)
   a = 2
-  if number <= 0 || number == 1
-    return false
-  elsif number == 2
+  if number <= 0 || number == 1 || number == 2
     return false
   else
     while i <= number/2
+    
 end
